5bcbbf9722e46a762f1ac198e504ac822f12b32b
JJ <87551031+RandomPerson1236@users.noreply.github.com>
2023-02-08 23:27:59 +0000

    F function
    if you can somehow find a use for this monstrous script, i'm proud of you.

---
+++ f.js
@@ -0,0 +1,48 @@
+// jQuery must be within your project for this script to function.
+
+function f ( x, n ){
+
+    let result = null;
+
+    for (let y = 0; y < x.length; y++){
+        if (typeof x[y] === 'number'){
+            if (y >= 5)throw new Error('Too many inputs');
+            result += '\n'+x[y] * n;
+        }
+        else throw new Error(`Inputs can only be a number! ${x} is invalid.`);
+    }
+
+    function debug( parent_element ){ 
+        parent_element = parent_element || null;
+
+        if (parent_element && typeof parent_element === 'string'){
+            
+            let el = $(`${parent_element}`);
+                
+            let splitResult = result.split('\n');
+            
+            for (let w = 0; w < splitResult.length; w++){
+                if (!splitResult[w].includes('null')) el.append(`<br><code>${splitResult[w]}</code>`);
+            }
+            
+            el.css({
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            });
+
+            $(`${parent_element} code`).css({ fontSize: '5rem' });
+
+        }
+        else if (parent_element && typeof parent_element != 'string') 
+            throw new Error(`${parent_element} is not a valid element.`);
+        else
+        {
+            return console.log(result);
+        }
+    }
+
+    return { debug };
+    
+}
+export { f };
